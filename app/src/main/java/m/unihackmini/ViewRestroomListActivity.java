8fa7b29421298367f4e10a055865f12d1cfd4b64
Tim Becker <timothy.r.becker@vanderbilt.edu>
2016-04-09 14:02:06 +1000

    Added View Restroom Activity

--- app/src/main/java/m/unihackmini/ViewRestroomListActivity.java
+++ app/src/main/java/m/unihackmini/ViewRestroomListActivity.java
@@ -3,12 +3,11 @@ package m.unihackmini;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class ViewRestroomListActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_view_restroom_list);
     }
-    
 }
