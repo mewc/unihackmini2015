d62e9a56682a7c2e0bbffa51606af5a892cb8987
darvid7 <bvdarvid@gmail.com>
2016-04-09 14:05:19 +1000

    Update 2
    Updated review/restroom

--- app/src/main/java/m/unihackmini/Restroom.java
+++ app/src/main/java/m/unihackmini/Restroom.java
@@ -7,49 +7,30 @@ import java.util.UUID;
  * Created by David on 9/04/16.
  */
 public class Restroom {
-    private ArrayList<ReviewAdvanced> reviewsAdvanced;
-    private ArrayList<ReviewStandard> reviewsStandard;
+    // restroom stats
+    private ArrayList<Review> reviews;
     private int bathroomRating;
     private int bathroomCleanliness;
     private int bathroomModernity;
     //private int bathroomTraffic;
+    private int abundanceTP;
+    private String dryer;
     private boolean wifi;
     private ArrayList<String> reviewComments;
 
     private String location;
-    private String getTo;
+    private String name;
     private int gender;
-    private Boolean disabled;
+    private Boolean hasDisabled;
 
-    public Restroom(String theLocation, String howGetTo, int theGender, Boolean hasDisabled) {
-        reviewsAdvanced = new ArrayList<>();
-        reviewsStandard = new ArrayList<>();
-        location = theLocation;
-        getTo = howGetTo;
-        gender = theGender;
-        disabled = hasDisabled;
+    public Restroom(String location, String name, int gender, Boolean hasDisabled) {
+        this.location = location;
+        this.gender = gender;
+        this.hasDisabled = hasDisabled;
     }
 
-    public void createReviewAdvanced() {
-        String newId = UUID.randomUUID().toString();
-        int theRating = 1;
-        int theCleanliness = 2;
-        int theModernity = 1;
-        int theTraffic = 1;
-        int theDuration = 15;
-        boolean hasWifi = true;
-        String theComment = "Nice";
-        String theUsername = "Anon";
-        int tpLvl = 5;
-        String theDryer = "New wind blowing machine";
 
-        ReviewAdvanced aReview = new ReviewAdvanced(tpLvl, theDryer, newId, theRating, theCleanliness, theModernity, theTraffic, theDuration, hasWifi, theComment, theUsername);
-
-        reviewsAdvanced.add(aReview);
-
-    }
-
-    public void createReviewStandard() {
+    public void testCreateReview() {
         String newId = UUID.randomUUID().toString();
         int theRating = 1;
         int theCleanliness = 2;
@@ -60,14 +41,14 @@ public class Restroom {
         String theComment = "Nice";
         String theUsername = "Anon";
 
-        ReviewStandard aReview = new ReviewStandard(newId, theRating, theCleanliness, theModernity, theTraffic, theDuration, hasWifi, theComment, theUsername);
+        Review aReview = new Review(newId, theRating, theCleanliness, theModernity, theTraffic, theDuration, hasWifi, theComment, theUsername);
 
-        reviewsStandard.add(aReview);
+        reviews.add(aReview);
 
     }
 
     public void upateStats() {
-        for (int i=0; reviewsStandard.size(); i++) {
+        for (int i=0; reviews.size(); i++) {
 
         }
     }
