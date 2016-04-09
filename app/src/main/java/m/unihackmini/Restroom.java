e0ae68c88230f64e8e92e4b439e9e477173121dc
darvid7 <bvdarvid@gmail.com>
2016-04-09 15:08:19 +1000

    update review/restroom
    update review/restroom

--- app/src/main/java/m/unihackmini/Restroom.java
+++ app/src/main/java/m/unihackmini/Restroom.java
@@ -7,51 +7,46 @@ import java.util.UUID;
  * Created by David on 9/04/16.
  */
 public class Restroom {
-    // restroom stats
+    // restroom details
+    private String name;
+    private String location;
+    private int gender;
+    private Boolean hasDisabled;
+    // restroom stats based off reviews
     private ArrayList<Review> reviews;
+    
     private int bathroomRating;
     private int bathroomCleanliness;
     private int bathroomModernity;
-    //private int bathroomTraffic;
-    private int abundanceTP;
+    private int tpAbundance;
     private String dryer;
     private boolean wifi;
     private ArrayList<String> reviewComments;
+    //private int bathroomTraffic;
 
-    private String location;
-    private String name;
-    private int gender;
-    private Boolean hasDisabled;
-
-    public Restroom(String location, String name, int gender, Boolean hasDisabled) {
+    public Restroom(String name, String location, int gender, Boolean hasDisabled) {
+        this.name = name;
         this.location = location;
         this.gender = gender;
         this.hasDisabled = hasDisabled;
     }
 
-
-    public void testCreateReview() {
-        String newId = UUID.randomUUID().toString();
-        int theRating = 1;
-        int theCleanliness = 2;
-        int theModernity = 1;
-        int theTraffic = 1;
-        int theDuration = 15;
-        boolean hasWifi = true;
-        String theComment = "Nice";
-        String theUsername = "Anon";
-
-        Review aReview = new Review(newId, theRating, theCleanliness, theModernity, theTraffic, theDuration, hasWifi, theComment, theUsername);
-
-        reviews.add(aReview);
-
+    public String getName() {
+        return name;
     }
 
-    public void upateStats() {
-        for (int i=0; reviews.size(); i++) {
-
-        }
+    public String getLocation() {
+        return location;
     }
+
+    public Boolean getHasDisabled() {
+        return hasDisabled;
+    }
+
+    public int getGender() {
+        return gender;
+    }
+
 }
 
 
--- app/src/main/java/m/unihackmini/Review.java
+++ app/src/main/java/m/unihackmini/Review.java
@@ -1,107 +1,110 @@
-    package m.unihackmini;
+package m.unihackmini;
 
-    /**
-     * Created by David on 9/04/16.
-     */
-    public class Review {
-        // quick review fields (7 items)
-        private String id;
-        private int rating;             // star rating
-        private int cleanliness;
-        private Boolean hasWifi;
-        private int duration;
-        private String comment;
-        private String username;        // can be anon
+/**
+ * Created by David on 9/04/16.
+ */
+public class Review {
+    // quick review fields (8 items)
+    private String id;
+    private int rating;             // star rating
+    private int cleanliness;
+    private Boolean hasWifi;
+    private int duration;
+    private String comment;
+    private String username;        // can be anon
+    private int odor;
 
-        // standard review  (9 items)
-        private int modernity;
-        private int traffic;
+    // standard review  (10 items)
+    private int modernity;
+    private int traffic;
 
-        // long review fields (11 items)
-        private int tpAbundance;
-        private String dryer;
+    // long review fields (12 items)
+    private int tpAbundance;
+    private String dryer;
 
-        // quick review constructor
-        public Review(String id, int rating, int cleanliness, int duration, Boolean hasWifi, String comment, String username) {
-            this.id = id;
-            this.rating = rating;
-            this.cleanliness = cleanliness;
-            this.duration = duration;
-            this.hasWifi = hasWifi;
-            this.comment = comment;
-            this.username = username;
-        }
-
-        // standard review constructor
-        public Review(String id, int rating, int cleanliness, int modernity, int traffic, int duration, Boolean hasWifi, String comment, String username) {
-            this.id = id;
-            this.rating = rating;
-            this.cleanliness = cleanliness;
-            this.modernity = modernity;
-            this.traffic = traffic;
-            this.duration = duration;
-            this.hasWifi = hasWifi;
-            this.comment = comment;
-            this.username = username;
-        }
-
-        // long review constructor
-        public Review(String id, int rating, int cleanliness, int modernity, int traffic, int duration, Boolean hasWifi, String comment, String username, int tpAbundance, String dryer) {
-            this.id = id;
-            this.rating = rating;
-            this.cleanliness = cleanliness;
-            this.modernity = modernity;
-            this.traffic = traffic;
-            this.duration = duration;
-            this.hasWifi = hasWifi;
-            this.comment = comment;
-            this.username = username;
-            this.tpAbundance = tpAbundance;
-            this.dryer = dryer;
-        }
-
-
-        public int getRating() {
-            return rating;
-        }
-
-        public Boolean getWifi() {
-            return hasWifi;
-        }
-
-        public int getCleanliness() {
-            return cleanliness;
-        }
-
-        public int getDuration() {
-            return duration;
-        }
-
-        public String getId() {
-            return id;
-        }
-
-        public int getModernity() {
-            return modernity;
-        }
-
-        public int getTraffic() {
-            return traffic;
-        }
-
-        public String getComment() {
-            return comment;
-        }
-
-        public String getUsername() {
-            return username;
-        }
-
-        public String description(){
-            return comment + "\n" + username + "duration: " + duration + ", Traffic:" + traffic + " Rating: " + rating;
-        }
-
+    // quick review constructor
+    public Review(String id, int rating, int cleanliness, int duration, Boolean hasWifi, String comment, String username,int odor) {
+        this.id = id;
+        this.rating = rating;
+        this.cleanliness = cleanliness;
+        this.duration = duration;
+        this.hasWifi = hasWifi;
+        this.comment = comment;
+        this.username = username;
+        this.odor = odor;
     }
+
+    // standard review constructor
+    public Review(String id, int rating, int cleanliness, int modernity, int traffic, int duration, Boolean hasWifi, String comment, String username, int odor) {
+        this.id = id;
+        this.rating = rating;
+        this.cleanliness = cleanliness;
+        this.modernity = modernity;
+        this.traffic = traffic;
+        this.duration = duration;
+        this.hasWifi = hasWifi;
+        this.comment = comment;
+        this.username = username;
+        this.odor = odor;
+    }
+
+    // long review constructor
+    public Review(String id, int rating, int cleanliness, int modernity, int traffic, int duration, Boolean hasWifi, String comment, String username, int tpAbundance, String dryer, int odor) {
+        this.id = id;
+        this.rating = rating;
+        this.cleanliness = cleanliness;
+        this.modernity = modernity;
+        this.traffic = traffic;
+        this.duration = duration;
+        this.hasWifi = hasWifi;
+        this.comment = comment;
+        this.username = username;
+        this.tpAbundance = tpAbundance;
+        this.dryer = dryer;
+        this.odor = odor;
+    }
+    
+    public int getRating() {
+        return rating;
+    }
+
+    public Boolean getWifi() {
+        return hasWifi;
+    }
+
+    public int getCleanliness() {
+        return cleanliness;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public int getModernity() {
+        return modernity;
+    }
+
+    public int getTraffic() {
+        return traffic;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String description(){
+        return comment + "\n" + username + "duration: " + duration + ", Traffic:" + traffic + " Rating: " + rating;
+    }
+
+}
 
 
 
